2bf8852575cd1877491bcb7d87a28ffefb763cab
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2024-12-25 14:14:44 +0100

    Update app.js
    change nr 17.jog  with nr 13.jpg

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Bildelister for hver mappe
     const imageLists = {
-        album1: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg","14.jpg","15.jpg","16.jpg", "17.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg"],
+        album1: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg","17.jpg","14.jpg","15.jpg","16.jpg","13.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg"],
         album2: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg","7.jpg","8.jpg", "9.jpg","10.jpg","11.jpg","12.jpg","13.jpg"],
         album3: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg","14.jpg","15.jpg"],
         //album4: ["1.jpg"]  //START SIDEEN FOR X-ANTALL MUZZIE ALBUM.

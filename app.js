eb496be8e1e5b8a1c9a4c92fcbfecf0847556016
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2024-12-25 16:30:54 +0100

    Update app.js
    adjust image array

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
         album2: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg","7.jpg","8.jpg", "9.jpg","10.jpg","11.jpg","12.jpg","13.jpg"],
         album3: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg","14.jpg","15.jpg","16.jpg"],
         //album4: ["1.jpg"]  //START SIDEEN FOR X-ANTALL MUZZIE ALBUM.
-        album4_1: ["1.jpg", "2.jpg"],
-        album4_2: ["1.jpg", "2.jpg", "3.jpg"],
-        album4_3: ["1.jpg", "2.jpg", "3.jpg"],
-        album5: ["1.jpg", "2.jpg", "3.jpg", "4.jpg","5.jpg", "6.jpg", "7.jpg", "8.jpg"],
+        album4_1: ["1.jpg","2.jpg","99.jpg","98.jpg",],
+        album4_2: ["1.jpg","2.jpg","3.jpg","4.jpg","6.jpg","5.jpg","7.jpg","8.jpg","9.jpg", "10.jpg"],
+        album4_3: ["1.jpg","2.jpg","3.jpg","4.jpg","6.jpg","5.jpg","7.jpg","8.jpg","9.jpg", "10.jpg"],
+        album5: ["1.jpg", "2.jpg", "3.jpg", "4.jpg","6.jpg","5.jpg","7.jpg","8.jpg","9.jpg", "10.jpg", "11.jpg", "12.jpg","17.jpg","14.jpg","15.jpg","16.jpg","13.jpg","18.jpg","19.jpg","20.jpg","21.jpg"],
         album6: ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg", "6.jpg", "7.jpg", "8.jpg"],
         album7: ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg", "6.jpg", "7.jpg", "8.jpg"],
    }

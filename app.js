62801d9352c8d7bfcf8bf9531d2eb1677d9ee91c
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2025-06-19 14:51:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ for (let i = 0; i < elements.length; i++) {
                 window.location.href = "album6/album6.html";
             } else if (accessCode === null || accessCode.trim() === "") {
                 // Håndter avbrutt prompt eller tomt svar
-                alert("Tomt ? \n Login-Secure: 2 feil gir virus i retur !");.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg","31.jpg","32.jpg","33.jpg","34.jpg","35.jpg","36.jpg","37.jpg","38.jpg", "39.jpg", "40.jpg","41.jpg","42.jpg","43.jpg","44.jpg","45.jpg","46.jpg","
+                alert("Tomt ? \n Login-Secure: 2 feil gir virus i retur !");
             } else {
                 // Feil kode
                 alert("Feil kode!  \n Login-Secure:2 feil gir virus i retur !");

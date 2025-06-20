3f90695f9f3956a30f92a7f89372a47e2104b0f4
CodeAndPranks <140973619+CodeAndPranks@users.noreply.github.com>
2025-06-20 15:26:39 -0700

    Update app.js
    mizzed 1-one eapostrophe :)

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ album4_1: [
               ],
         
         album6: ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg", "6.jpg", "7.jpg", "8.jpg"],
-       album7: ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg", "6.jpg", "7.jpg", "8.jpg",8.jpg","9jpg","10.jpg", "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg", "24.jpg","25.jpg]
+       album7: ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg", "6.jpg", "7.jpg", "8.jpg",8.jpg","9jpg","10.jpg", "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg", "24.jpg","25.jpg"]
    }
     // Hvis "photo" ikke fins i albumet, avslutt funksjonen.
  if (!photo || !imageLists[folderName]) {
